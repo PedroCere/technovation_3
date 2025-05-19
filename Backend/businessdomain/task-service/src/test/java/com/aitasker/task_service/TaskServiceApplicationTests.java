4d0c7aae6ef5af48e7e9c6c5996c39870e23715c
DMateo <93503252+Teobac123@users.noreply.github.com>
2025-05-19 15:06:46 -0300

    Add backend structure to project

---
+++ Backend/businessdomain/task-service/src/test/java/com/aitasker/task_service/TaskServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aitasker.task_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
