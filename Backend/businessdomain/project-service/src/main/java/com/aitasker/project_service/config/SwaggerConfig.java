4fc9255ec81b421b787903548181486247f9532b
DMateo <93503252+Teobac123@users.noreply.github.com>
2025-05-30 16:57:22 -0300

    Add config to project-service

---
+++ Backend/businessdomain/project-service/src/main/java/com/aitasker/project_service/config/SwaggerConfig.java
@@ -0,0 +1,19 @@
+package com.aitasker.project_service.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Project Service API")
+                        .version("1.0")
+                        .description("API para gestionar proyectos que no vas a terminar a tiempo."));
+    }
+}
